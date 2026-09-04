4bb4e4e9162eb16717cb4227f5497c521aaecde2
Faisal Abbas <kaze0001111@gmail.com>
2026-09-04 15:24:52 +0500

    feat: update planner based on router node ;

--- backend/src/backend/agent/planner.py
+++ backend/src/backend/agent/planner.py
@@ -5,21 +5,17 @@ planner_model = model.with_structured_output(ResearchPlan)
 
 
 async def planner(state):
-   response = await planner_model.ainvoke(
-      f"""
+    response = await planner_model.ainvoke(
+        f"""
         You are a research planner.
 
-        Break the user's question into a small number of
-        concrete research tasks.
-
-        Each task should investigate one specific aspect
-        required to answer the question accurately.
+        - For casual conversation or simple tasks, return no tasks.
+        - Otherwise, break the question into a small number of specific research tasks.
+        - Do not create unnecessary tasks.
 
         User question:
         {state["query"]}
         """
-   )
+    )
 
-   return {
-      "plan": response.tasks
-   }
+    return {"plan": response.tasks}
